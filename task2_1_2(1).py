04f8e662a8e0503fb288d65af841a926f75e9405
FERNANDODORO <32836105+MaChengYuan@users.noreply.github.com>
2022-09-14 01:54:49 +0800

    Add files via upload

---
+++ task2_1_2(1).py
@@ -0,0 +1,93 @@
+
+from scipy import misc
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import optimize
+ 
+# objective function
+def func(x):
+	return abs(x-0.2) 
+
+class gauss:3
+    counter = 0
+    def __call__(self,a , b):
+        
+        start = a 
+        end = b
+        alpha = 0.1 # learning rate
+        nb_max_iter = 100 # Nb max d'iteration
+        eps = 0.0001 # stop condition
+        
+        x0 = start # start point
+        y0 = func(x0)
+        
+        
+        cond = eps + 10.0 # start with cond greater than eps (assumption)
+        nb_iter = 0 
+        tmp_y = y0
+        while cond > eps and nb_iter < nb_max_iter and x0 >= start and x0 <= end:
+            x0 = x0 - alpha * misc.derivative(func, x0)
+            y0 = func(x0)
+            gauss.counter += 1
+            nb_iter = nb_iter + 1
+            cond = abs( tmp_y - y0 )
+            tmp_y = y0
+        
+        print('iteration is  : %.0f'%nb_iter)
+        print ('lowest point %f,%f'%(x0,y0))
+        print('evaluation of objective function is : %.0f'%gauss.counter)
+        plt.scatter(x0, y0)
+        plt.grid()
+    
+        plt.title("Gradient Descent Python (1d test)")
+
+
+class nelder:
+    counter = 0
+    def __call__(self,a,b):
+        nelder.counter += 1
+        x0 = a
+        ans = optimize.minimize(func, x0)
+        minX = optimize.minimize(func, x0).x
+        minY = func(minX)
+        print(ans)
+        plt.plot(minX , minY,'r*',label = '$lower point$')
+        
+        plt.title("Nelder-Mead")
+        print("iteration is : %.f"%ans.nit)
+        print('evaluation of objective function is : %.0f'%ans.nfev)
+        plt.legend()    
+        plt.grid()
+        plt.show()
+        
+
+    
+
+  
+def main():
+    a = 1e-6
+    b = 1
+    
+    x = np.arange(a,b,1e-6)
+    y = func(x)
+    plt.subplot(121)
+    plt.plot(x,y)    
+    Nelder = nelder()
+    Nelder(a, b)
+    print('nelder is called : %.0f'%Nelder.counter)
+    
+    print('\n')
+    
+    
+    plt.subplot(122)
+    plt.plot(x,y)
+    Gauss = gauss()
+    Gauss(a, b)
+    
+    
+ 
+      
+  
+if __name__ == "__main__":
+    main()   
+
---
+++ task2_1_3.py
@@ -0,0 +1,167 @@
+import numpy as np
+import math
+from  matplotlib import pyplot as plt
+from scipy.optimize import minimize
+from numpy.random import rand
+from numpy.random import randn
+
+
+class bisection:
+    counter = 0
+    def __call__(self,a,b):
+     
+        iter = 0
+        while ((b-a) >= 0.001):
+            iter = iter + 1 
+            # Find middle point
+            c = (a+b)/2
+      
+            
+            if (equation(c) <= equation(a) and equation(c) >= equation(b)):
+                bisection.counter += 4
+                a = c
+            elif(equation(c) <= equation(b) and equation(c) >= equation(a)):
+                bisection.counter += 4
+                b = c
+            else:
+                if(equation(a)>=equation(b)):
+                    bisection.counter += 2
+                    a = c
+                else:
+                    b = c
+        print('iteration is  : %.0f' %iter) 
+        print("The value of root is : ","%.4f"%c)
+        
+        plt.plot(c,equation(c),'r*',color='red')
+        bisection.counter += 1
+        print('evaluation of objective function is : %.0f'%bisection.counter)
+        print('\n\n')
+        plt.title("Bisection")  
+     
+
+
+def equation(x):
+    return np.sin(1/x)
+
+
+
+class exhaustive:
+    counter = 0 
+    def __call__(self,a , b , n):
+        x1 = a
+        dx = (b-a)/n 
+        x2 = x1 + dx
+        x3 = x2 + dx
+        iter = 0
+        while (x3 <= b):
+            iter = iter + 1 
+            y1 = equation(x1)
+            y2 = equation(x2)
+            y3 = equation(x3)
+            exhaustive.counter += 3 
+            if y1>=y2 and y2<=y3:
+                
+                 print ('The minimum point lies between ',x1,' & ',x3)
+                 plt.plot(x1,equation(x1),'r*',label = '$lower point$')
+                 exhaustive.counter += 1
+                 print('evaluation of objective function is : %.0f'%exhaustive.counter)
+                 
+              
+                 break
+            else:
+                x1 = x2
+                x2 = x3
+                x3 = x2 + dx
+                if(x3 > b):
+                    print('bounday is minimun')
+                    plt.plot(a,equation(a),'r*',label = '$lower point$')
+                    exhaustive.counter+= 1
+                    print('evaluation of objective function is : %.0f'%exhaustive.counter)
+                    print('\n\n')
+                    break
+        plt.title("Exhaustive")  
+        print('iteration is  : %.0f' %iter) 
+        print('\n\n')
+        
+class golden_section:
+    counter = 0
+    def __call__(self,a , b):
+            iter = 0 
+            gr = (math.sqrt(5)-1)/2
+            start = a
+            end = b
+            x = np.arange(start,end,1e-6)
+            y = equation(x) 
+            golden_section.counter += 1
+            while(abs(end-start) >= 0.001):
+                iter += 1
+               
+                d = gr*(end-start)
+                x1 = d+start
+                x2 = end-d
+               
+                if(equation(x1) > equation(x2)):
+                    golden_section.counter += 2
+                    end = x1 
+                elif (equation(x1)==equation(x2)):
+                    golden_section.counter += 2
+                    break
+                else:
+                    start = x2
+        
+        
+            plt.plot(x,y,label='function')  
+            plt.plot(start,equation(start),'r*',label = '$lower point$')
+            golden_section.counter += 1
+            print('evaluation of objective function is : %.0f'%golden_section.counter)
+            print('iteration is  : %.0f' %iter) 
+            plt.legend()
+            print('\n\n')
+            plt.title("Golden_section")  
+           
+           
+            return a
+
+
+
+
+  
+def main():
+    a = 1e-6
+    b = 1
+    n = 1000
+    
+    x = np.arange(a,b,1e-6)
+    y = equation(x)
+    plt.subplot(131)
+    plt.plot(x,y)    
+    Bisec = bisection()
+    Bisec(a, b)
+    
+
+    plt.subplot(132)
+    
+    plt.plot(x,y)
+    Exhaustive = exhaustive()
+    Exhaustive(a,b,n)
+    
+    
+    plt.subplot(133)
+
+    plt.plot(x,y)
+    Gold = golden_section()
+    Gold(a,b)
+    
+
+
+
+    
+    
+    
+    
+      
+  
+if __name__ == "__main__":
+    main()    
+
+
